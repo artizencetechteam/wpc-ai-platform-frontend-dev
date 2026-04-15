8f74c938c273d6be7e135b2338cab9492eca1fd4
Aditya30ag <adityaagrwal3005@gmail.com>
2026-04-15 17:41:44 +0530

    change the route

--- app/employer/_components/Sidebar.tsx
+++ app/employer/_components/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
 
   const routes = [
     { id: 1, name: 'Dashboard', route: '/employer/dashboard', icon: LayoutDashboard },
-    { id: 3, name: 'Post Compliance', route: '/employer/dashboard/post-role', icon: Briefcase },
+    { id: 3, name: 'Post Compliance', route: '/employer/dashboard/post-compliance', icon: Briefcase },
     { id: 2, name: 'HR Validation', route: '/employer/sections/hr-validation', icon: Users },
     { id: 4, name: 'Call Agents', route: '/employer/dashboard/call-agents', icon: CalendarCheck },
     { id: 5, name: 'Staff List', route: '/employer/dashboard/staff-list', icon: CalendarCheck },
